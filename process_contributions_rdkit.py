Repaired bug with varying count of models. Repaired bug with fragments contributions - fragment for every model, now I average them. In process_predictions.py I use only one method for make decisions : filtering, pareto, desirability.

process_contributions_rdkit.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
 
 from rdkit import Chem
-from process_predictions import parse_threshold
+from process_predictions import parse_threshold_pareto_filtering
 import math
 import numpy as np
+from collections import defaultdict
+
 
 def get_ranges():
     """
@@ -57,41 +59,76 @@ def compute_normalized_value(in_value, predicted_value, threshold, range):
             return 2 * ((1 / (1 + math.exp((7 / range) * -in_value))) - 1) + 1
 
 
-def normalize_contributions(in_fname, parameters, predicted_values, thresholds, ranges):
-    with open(in_fname, 'a') as in_file:
-        list_of_contributions = list()
-        for number_of_parameter, parameter in enumerate(parameters):
-            with open('contrib_' + parameter + '.txt', 'r') as contrib_f:
-                string_to_write_to_file = ''
-                contrib_f.readline()
+def compute_average_from_models(frags, models):
+    for compound_id, frag_id in frags.items():
+        for frag_id, values in frag_id.items():
+            property_avg = sum(values[-len(models):])/len(models)
+            for i in range(len(models)):
+                frags[compound_id][frag_id].pop()
+            frags[compound_id][frag_id].append(property_avg)
+    return frags
 
-                for line_number, line in enumerate(contrib_f.readlines()):
-                    line = line.split('\t')
+
+def get_string_of_dict(frags):
+    number_of_fragments = 0
+    output_string = ""
+    for compound_id, frag_id in frags.items():
+        for frag_id, values in frag_id.items():
+            output_string += compound_id + '\t' + frag_id + '\t'
+            output_string += \
+                ''.join([str(norm_value) + '\t' for norm_value in frags[compound_id][frag_id]])
+            output_string += '\n'
+            number_of_fragments += 1
+    return output_string, number_of_fragments
+
+
+def normalize_contributions(in_fname, parameters, predicted_values, thresholds, ranges, models):
+    with open(in_fname, 'a') as in_file:
+
+        frags = defaultdict(dict)
+
+        with open('contrib_' + parameters[0] + '.txt', 'r') as contrib_f:
+            contrib_f.readline()
+            for line in contrib_f:
+                line = [l.strip() for l in line.split('\t')]
+
+                norm_value = compute_normalized_value(
+                    float(line[5]),  # fragment contributions
+                    predicted_values[line[0]][parameters[0]],  # predicted parameter value of compound
+                    thresholds[0],  # threshold for parameter
+                    ranges[parameters[0]])  # range of parameter
+
+                try:
+                    frags[line[0]][line[2]].append(norm_value)
+                except:
+                    frags[line[0]][line[2]] = []
+                    frags[line[0]][line[2]].append(norm_value)
+
+            frags = compute_average_from_models(frags, models[0])
+
+        for i, parameter in enumerate(parameters[1:]):
+            file_name = 'contrib_' + parameter + '.txt'
+            with open(file_name, 'r') as contrib_f:
+                contrib_f.readline()
+                for line in contrib_f:
+                    line = [l.strip() for l in line.split('\t')]
 
                     norm_value = compute_normalized_value(
-                                    float(line[5]),                         # fragment contributions
-                                    predicted_values[line[0]][parameter],   # predicted parameter value of compound
-                                    thresholds[number_of_parameter],        # threshold for parameter
-                                    ranges[parameter])                      # range of parameter
+                        float(line[5]),  # fragment contributions
+                        predicted_values[line[0]][parameter],  # predicted parameter value of compound
+                        thresholds[i+1],  # threshold for parameter
+                        ranges[parameter])  # range of parameter
 
-                    if number_of_parameter == 0:
-                        list_of_contributions.append([norm_value])
-                    else:
-                        list_of_contributions[line_number].append(norm_value)
+                    frags[line[0]][line[2]].append(norm_value)
+                frags = compute_average_from_models(frags, models[i + 1])
 
-                    # if it is last parameter, we need to compute average of normalized values and construct string
-                    # which will be added to file
-                    if number_of_parameter == len(parameters) - 1:
-                        string_to_write_to_file = line[0] + '\t' + line[2] + '\t'
-                        string_to_write_to_file += \
-                            ''.join([str(norm_value) + '\t' for norm_value in list_of_contributions[line_number]])
-                        average = sum(list_of_contributions[line_number])/len(list_of_contributions[line_number])
-                        string_to_write_to_file += str(average)
-
-                        in_file.write(string_to_write_to_file + '\n')
-                        string_to_write_to_file = ''
-                number_of_fragments = line_number + 1
-    return line_number
+        for compound_id, frag_id in frags.items():
+            for frag_id, values in frag_id.items():
+                property_avg = sum(values[-len(parameters):]) / len(parameters)
+                frags[compound_id][frag_id].append(property_avg)
+        output_string, number_of_fragments = get_string_of_dict(frags)
+        in_file.write(output_string)
+    return number_of_fragments
 
 
 
@@ -100,7 +137,7 @@ def pick_worst_ones(in_fname, number_of_fragments, number_of_compounds):
     normalized_array = np.genfromtxt(
         in_fname,
         skip_header=1,
-        usecols=(0, 1, -1),
+        usecols=(0, 1, -2),
         dtype=None,
         names=['compound_id', 'fragment_id', 'average_contrib'],
         delimiter='\t')
@@ -124,14 +161,15 @@ def pick_worst_ones(in_fname, number_of_fragments, number_of_compounds):
 
 
 in_fname = 'fragment_contrib_norm.txt'
+models = [['svm'], ['rf', 'svm']]
 parameters = ['LOGBB', 'solubility']
 pred_file = 'output_pareto.sdf'
 thresholds =['more0.5', 'more-2']
 
 processed_file = create_file_with_processed_data(in_fname, parameters)
 predicted_values, number_of_compounds = get_predicted_value_for_whole_compound(pred_file, parameters)
-thresholds = parse_threshold(thresholds)
-number_of_fragments = normalize_contributions(in_fname, parameters, predicted_values, thresholds, get_ranges())
+thresholds = parse_threshold_pareto_filtering(thresholds)
+number_of_fragments = normalize_contributions(in_fname, parameters, predicted_values, thresholds, get_ranges(), models)
 
 # we can't specify more worst fragments then we have
 number_of_worst_fragments = 10

process_predictions.py
@@ -5,6 +5,9 @@ import argparse
 import numpy as np
 import pareto_simple_cull as pareto_alg
 
+from sympy import symbols
+from sympy.parsing.sympy_parser import parse_expr
+
 
 # --------------------------Saving outputs--------------------------
 # save output dictionary to file with predicted values
@@ -14,7 +17,7 @@ import pareto_simple_cull as pareto_alg
 #               input_dict: output dictionary where are selected compounds stored,
 #                            e.g. {'filtering': [[id1, predicted_value1, predicted_value2],[...]], 'pareto':[[...]]}
 #               predictions: list of parameters name, e.g. ['logBB', 'solubility', ...]
-def save_output(input_sdf, output_file, input_dict, predictions):
+def save_output(input_sdf, output_file, input_dict, parameters_to_predict):
     output_string = ''
     # prepare list of files
     list_of_files = []
@@ -31,7 +34,7 @@ def save_output(input_sdf, output_file, input_dict, predictions):
                     for line in iter_file:
                         line = line.rstrip()
                         if '$$$$' in line:
-                            for index_of_predictions, predict in enumerate(predictions):
+                            for index_of_predictions, predict in enumerate(parameters_to_predict):
                                 output_string += '>  <' + predict + '>\n' + str(item[1 + index_of_predictions]) + '\n\n'
                             output_string += line + '\n'
                             break
@@ -56,21 +59,24 @@ def save_output(input_sdf, output_file, input_dict, predictions):
 # output format: return prepared array in format [id, predicted_value1, predicted_value2, ...]
 
 # if ad is specified, return only compounds which are in ad
-def prepare_array(input_pred, bounded_box, predictions):
-    in_data = np.genfromtxt(input_pred, dtype=None, delimiter='\t')
+def prepare_array(input_pred, bounded_box, parameters_to_predict):
+    # load and prepare data to numpy array
+    working_list = []
+    with open(input_pred, 'r') as in_f:
+        for line in in_f:
+            if '#' in line:
+                pass
+            else:
+                line = [cell.strip() for cell in line.split('\t')]
+                working_list.append([int(line[0])])
+                working_list[-1].append(float(line[-2]))
 
-    working_arr = np.asarray([[in_data[0][0], in_data[0][-2], in_data[0][-1]]])
-    for item in in_data[1:]:
-        working_arr = np.append(working_arr, [[item[0], item[-2], item[-1]]], axis=0)
-
-    # -2 prediction column, -1 bounded box column
-    if bounded_box:
-        working_arr = working_arr[:, [0, -2, -1]]
-    else:
-        working_arr = working_arr[:, [0, -2]]
+                if bounded_box:
+                    working_list[-1].append(line[-1] == 'True')
+    working_arr = np.array(working_list)
 
     # split data after all predictions
-    working_arr = np.split(working_arr, len(predictions), axis=0)
+    working_arr = np.split(working_arr, len(parameters_to_predict), axis=0)
 
     # add to final_array first column with ids
     final_ar = working_arr[0]
@@ -84,9 +90,9 @@ def prepare_array(input_pred, bounded_box, predictions):
 
     # filter array if bound_box
     if bounded_box:
-        for i in range(len(predictions)):
+        for i in range(len(parameters_to_predict)):
             final_ar = final_ar[final_ar[:, 2 * i + 2] == 1]
-        return final_ar[:, [0] + [i * 2 + 1 for i in range(len(predictions))]]
+        return final_ar[:, [0] + [i * 2 + 1 for i in range(len(parameters_to_predict))]]
     else:
         return final_ar
 
@@ -94,17 +100,23 @@ def prepare_array(input_pred, bounded_box, predictions):
 # convert input thresholds to parsed 2D list
 # input format: thresholds: list of thresholds, e.g. ['more4', 'betwenn-0.5to1', less'-2']
 # output format: return list of parsed threshold, e.g. [['more',4], ['between', -0.5, 1], ['less', -2]]
-def parse_threshold(thresholds):
+def parse_threshold_pareto_filtering(thresholds):
     threshold_match = []
     for threshold in thresholds:
         if 'less' in threshold:
             threshold_match.append(['less', float(threshold[4:])])
         elif 'more' in threshold:
             threshold_match.append(['more', float(threshold[4:])])
-        else:
+        elif 'between' in threshold:
             threshold_match.append(
                 ['between', float(threshold[7:].split('to')[0]), float(threshold[7:].split('to')[1])])
     return threshold_match
+
+
+def parse_threshold_desirability(thresholds):
+    for threshold in thresholds:
+        if 'desirability' in threshold:
+            return threshold.split('#')[0].split('_')[1:], int(threshold.split('#')[1][0])
 
 
 # --------------------------Filtering--------------------------
@@ -154,7 +166,7 @@ def get_distance_from_threshold(col, threshold_type, threshold_value1, threshold
 #               thresholds: parsed thresholds with format e.g. [['more',4], ['between', -0.5, 1], ['less', -2]]
 #               predictions: list of parameters which you want to predict, e.g. ['logBB', 'solubility']
 # output format: return array of compounds which lies on pareto frontier in same format as working_ar
-def pareto(input_sdf, working_ar, thresholds, predictions):
+def pareto(input_sdf, working_ar, thresholds, parameters_to_predict):
     # vectorize calculation of distances
     get_distance = np.vectorize(get_distance_from_threshold, otypes=[np.float64])
 
@@ -179,7 +191,7 @@ def pareto(input_sdf, working_ar, thresholds, predictions):
     # if some compounds match the threshold
     if len(output) != 0:
         match_threshold_output['pareto'] = output
-        save_output(input_sdf, 'output_match.sdf', match_threshold_output, predictions)
+        save_output(input_sdf, 'output_match.sdf', match_threshold_output, parameters_to_predict)
         # clear the output
         output = []
         # remove rows which are in output
@@ -199,23 +211,73 @@ def pareto(input_sdf, working_ar, thresholds, predictions):
     return output
 
 
-def main_params(input_sdf, input_pred, predictions, output_file, methods, thresholds, use_bounded_box):
-    # preparing variables
+# --------------------------Desirability functions--------------------------
+def process_function(in_function):
+    functions = []
+    for fnc in in_function:
+        fnc = fnc.split(',')
+        for index, fun in enumerate(fnc):
+            fnc[index] = [fun.split(":")[0]]  + [parse_expr(fun.split(":")[1])]
+        functions.append(fnc)
+    return functions
 
-    # parse threshold
-    threshold_match = parse_threshold(thresholds)
+
+def get_norm_value(function, x_input):
+    x = symbols("x")
+    for index, bound in enumerate(function):
+        if round(x_input, 5) <= float(bound[0]): return float(function[index][1].subs(x, x_input))
+    return 0
+
+
+def desirability(input_sdf, working_ar, threshold_filtering, threshold_desire, number_of_compounds, parameters_to_predict):
+
+    # filter compounds which match the threshold
+    match_threshold_output = {}
+    output = filtering(working_ar.copy(), threshold_filtering)
+    match_threshold_output['match'] = output
+    save_output(input_sdf, 'output.sdf', match_threshold_output, parameters_to_predict)
+
+    # delete filtered compounds from working array
+    for id in output[:, 1]:
+        working_ar = working_ar[working_ar[:, 1] != id]
+
+    working_ar = np.hstack((working_ar, np.zeros((working_ar.shape[0], 1))))
+    functions = process_function(threshold_desire)
+
+    if number_of_compounds > working_ar.shape[0]:
+        number_of_compounds = working_ar.shape[0]
+
+    number_of_parameters = len(functions)
+    for index, row in enumerate(working_ar):
+        row_average = 0
+        for predictions, fnc in zip(row[1:-1], functions):
+            row_average += get_norm_value(fnc, predictions)
+        working_ar[index, -1] = row_average / number_of_parameters
+
+    tmp_arr = working_ar[working_ar[:, -1].argsort()][::-1][:number_of_compounds, :-1]
+    return tmp_arr[tmp_arr[:, 0].argsort()]
+
+def main_params(input_sdf, input_pred, parameters_to_predict, output_file, methods, thresholds, use_bounded_box):
+
+    # preparing threshold for filtering or pareto
+    threshold_match = parse_threshold_pareto_filtering(thresholds.copy())
+
+    #prepare array
+    working_array = prepare_array(input_pred, use_bounded_box, parameters_to_predict)
 
     # output dictionary
     output = {}
+
     if 'filtering' in methods:
-        working_array = prepare_array(input_pred, use_bounded_box, predictions)
-        output['filtering'] = filtering(working_array, threshold_match)
-    if 'pareto' in methods:
-        working_array = prepare_array(input_pred, use_bounded_box, predictions)
-        output['pareto'] = pareto(input_sdf, working_array, threshold_match, predictions)
+        output['process_predictions'] = filtering(working_array, threshold_match)
+    elif 'pareto' in methods:
+        output['process_predictions'] = pareto(input_sdf, working_array, threshold_match, parameters_to_predict)
+    else: # desirability
+        threshold_desire, number_of_compounds = parse_threshold_desirability(thresholds)
+        output['process_predictions'] = desirability(input_sdf, working_array, threshold_match, threshold_desire, number_of_compounds, parameters_to_predict)
 
     # save outputs to coresponding files
-    save_output(input_sdf, output_file, output, predictions)
+    save_output(input_sdf, output_file, output, parameters_to_predict)
 
 
 def main():