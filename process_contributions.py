47122e952d5ca323a9593becc523402bc5e5a005
MariBerry <yurievnamaria@gmail.com>
2025-05-26 14:05:57 +0200

    Revert "instead of abs now use relu before normalizing contrbuitons"
    This reverts commit dd5fb070b199074cd8de609bdc291545ed2e84fd.

--- process_contributions.py
+++ process_contributions.py
@@ -90,14 +90,14 @@ def compute_normalized_value(record: pandas_series_row, predictions: pandas_tabl
 
     if threshold[0] == 'more':
         if threshold[1] <= predicted_value:
-            x = max(0, x) # relu to make negative sme as zero
+            x = abs(x)
             return 2 * ((1 / (1 + math.exp((7 / range) * - x))) - 1) + 1
         else:
             return 2 * ((1 / (1 + math.exp((7 / range) * - x))) - 1) + 1
     elif threshold[0] == 'less':
         if threshold[1] >= predicted_value:
-            x = min(0, x)# to make positive same as zero
-            return -(2 * ((1 / (1 + math.exp((7 / range) * - x))) - 1) + 1)
+            x = abs(x)
+            return 2 * ((1 / (1 + math.exp((7 / range) * - x))) - 1) + 1
         else:
             return -(2 * ((1 / (1 + math.exp((7 / range) * - x))) - 1) + 1)
     # between
